8bb0dff00f192c049bd046b0f1fa268f3f3aa544
Evgenija123 <evgenija.divitarova1@gmail.com>
2024-06-06 14:24:52 +0200

    12

--- app/src/main/java/com/example/kviz/MainActivity.kt
+++ app/src/main/java/com/example/kviz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kviz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kviz.databinding.ActivityMainBinding
 import com.google.firebase.database.FirebaseDatabase
@@ -21,12 +22,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecyclerView() {
+        binding.progressBar.visibility= View.GONE //koga ke se zemat podatocite da go nema progres barot
         adapter = QuizListAdapter(quizModelList)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
     }
 
     private fun getDataFromFirebase() {
+        binding.progressBar.visibility= View.VISIBLE //koga se zemaat podatocite progres barot da se gleda
         FirebaseDatabase.getInstance().reference
             .get() // Get the data
             .addOnSuccessListener { dataSnapshot ->
