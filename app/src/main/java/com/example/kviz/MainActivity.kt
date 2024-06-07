4c525b5899d1ebdd4c77ce934e0a1f94bb88068a
Evgenija123 <evgenija.divitarova1@gmail.com>
2024-06-08 00:24:52 +0200

    19
     Notifications

--- app/src/main/java/com/example/kviz/MainActivity.kt
+++ app/src/main/java/com/example/kviz/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private val quizModelList: MutableList<QuizModel> = mutableListOf()
     private lateinit var adapter: QuizListAdapter
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
