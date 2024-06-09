77543daec9ad68923af00bd40d6a038e03b044b0
Evgenija123 <evgenija.divitarova1@gmail.com>
2024-06-09 22:10:52 +0200

    21

--- app/src/main/java/com/example/kviz/MainActivity.kt
+++ app/src/main/java/com/example/kviz/MainActivity.kt
@@ -9,6 +9,8 @@ import com.example.kviz.databinding.ActivityMainBinding
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 
+
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val quizModelList: MutableList<QuizModel> = mutableListOf()
